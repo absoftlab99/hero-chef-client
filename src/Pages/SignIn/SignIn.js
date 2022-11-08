eb86ed71bac095c7f7068488c7805e5075a892f0
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-11-09 01:05:19 +0600

    login form completed

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import login from '../../images/login.gif';
 
 const SignIn = () => {
     return (
-        <div>
-            <h2>This is signin page</h2>
+        <div className='container p-5'>
+            <div className="row w-75 mx-auto">
+                <div className="col-6 border-end">
+                    <h2 className='text-center ff-poppins'>Login Form</h2>
+                    <img className='img-fluid' src={login} alt="" />
+                </div>
+                <div className="col-6 p-5 ff-poppins my-auto">
+                    <Form>
+                        <Form.Group className="mb-2" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control name='email' type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-2" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control name='password' type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-2" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me" />
+                        </Form.Group>
+                        <Button variant="info" type="submit">
+                            Sign In
+                        </Button>
+                    </Form>
+                </div>
+            </div>
         </div>
     );
 };
